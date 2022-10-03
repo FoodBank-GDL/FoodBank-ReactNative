2c9217b16766f11f54c3c1b207456cb288a7e6eb
CristianCazares <cm_cjavier@hotmail.com>
2022-10-03 14:25:21 -0500

    feat(client): created login and register places holders and set up of App.js

---
+++ client/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text, StyleSheet } from "react-native";
+
+import Login from "./src/screens/Login";
+import Register from "./src/screens/Register";
+const Stack = createNativeStackNavigator();
+export default function App() {
+  //Push notifs
+
+  //Global state management
+
+  //Navigation
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" options={{ headerShown: false }}>
+          {(props) => <Login {...props} />}
+        </Stack.Screen>
+        <Stack.Screen name="Register">
+          {(props) => <Register {...props} options={{ headerShown: false }} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
