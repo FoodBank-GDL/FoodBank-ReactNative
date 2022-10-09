689f324dd94d013688d2754e8f49797d8e15cbe3
Emilio Gonzalez <emiliogonmar@Emilios-MacBook-Pro-2.local>
2022-10-09 15:20:19 -0500

    feat(NavBar): NavBar icons

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,68 @@
+import { StyleSheet, View, TouchableOpacity } from "react-native";
+
+import IconE from "react-native-vector-icons/Entypo"
+import IconFA from "react-native-vector-icons/FontAwesome"
+
+const NavBar = ({ navigation }) => {
+
+    const handleNavigate = () => {
+
+    }
+
+    return (
+        <View style={Styles.container}>
+
+            <View style={Styles.iconContainer}>
+                <TouchableOpacity
+                    onPress={() => handleNavigate("Feed")}
+                >
+                    <IconE
+                        name="home"
+                        style={Styles.icon}
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    onPress={() => handleNavigate("Login")}
+                >
+                    <IconFA
+                        name="map"
+                        style={Styles.icon}
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    onPress={() => handleNavigate("Profile")}
+                >
+                    <IconFA
+                        name="user"
+                        style={Styles.icon}
+                    />
+                </TouchableOpacity>
+
+            </View>
+
+        </View>
+    );
+};
+
+const Styles = StyleSheet.create({
+    container: {
+        height: 80,
+        backgroundColor: 'orange',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+    },
+    iconContainer: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        height: "70%",
+        alignItems: "center"
+    },
+    icon: {
+        fontSize: 25,
+        color: "white"
+    }
+})
+
+export default NavBar;
