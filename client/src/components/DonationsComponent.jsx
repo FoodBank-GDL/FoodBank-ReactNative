ef27aa42e96f8790dfcffde3f9e415ae0c2edf6b
Emilio Gonzalez <emiliogonmar@Emilios-MacBook-Pro-2.local>
2022-10-10 13:59:48 -0500

    feat(donations): donations status description

--- client/src/components/DonationsComponent.jsx
+++ client/src/components/DonationsComponent.jsx
@@ -5,10 +5,43 @@ import {
 } from "react-native";
 import DonationCard from "./DonationCard";
 
+import IconMCI from "react-native-vector-icons/MaterialCommunityIcons"
+
 const DonationsComponent = ({ }) => {
 
     return (
         <View style={Styles.container}>
+
+            <View style={Styles.statusDescription}>
+                <View style={Styles.text}>
+                    <Text style={Styles.statusDescriptionText}>Estado del donativo:</Text>
+                </View>
+
+                <View style={Styles.status}>
+
+                    <View style={Styles.statusCategory}>
+                        <IconMCI
+                            name="checkbox-blank-circle"
+                            size={17}
+                            color={"#FE4C4C"}
+                        />
+                        <Text style={Styles.statusDescriptionText}>Pendiente</Text>
+
+                    </View>
+
+
+                    <View style={Styles.statusCategory}>
+                        <IconMCI
+                            name="checkbox-blank-circle"
+                            size={17}
+                            color={"#8BE794"}
+                        />
+                        <Text style={Styles.statusDescriptionText}>Completado</Text>
+
+                    </View>
+                </View>
+            </View>
+
             <DonationCard
                 name={"Rafael Rodriguez"}
                 status={"pending"}
@@ -35,6 +68,21 @@ export const Styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "white",
         padding: 20
+    },
+    statusDescription: {
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    statusDescriptionText: {
+        fontSize: 12,
+    },
+    status: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        width: 200
+    },
+    statusCategory: {
+        flexDirection: "row"
     }
 })
 
