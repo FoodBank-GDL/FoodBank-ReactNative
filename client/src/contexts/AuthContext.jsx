b35512a0cac5b43139a1580b3144971da5af8530
CristianCazares <cm_cjavier@hotmail.com>
2022-10-12 12:56:49 -0500

    feat(client/auth): added a context for authentication

---
+++ client/src/contexts/AuthContext.jsx
@@ -0,0 +1,64 @@
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const INITIAL_AUTH = { idToken: "", userId: "" };
+
+const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState(INITIAL_AUTH);
+
+  const isAuthenticated = () => !!auth.idToken && !auth.userId;
+
+  const storeCredentials = async (idToken, userId) => {
+    await AsyncStorage.setItem("idToken", idToken);
+    await AsyncStorage.setItem("userId", userId);
+
+    setAuth({ idToken, userId });
+  };
+
+  const logout = () => {
+    AsyncStorage.clear();
+    setAuth(INITIAL_AUTH);
+    //TODO: NAVIGATE TO LOGIN
+    console.log("LOGOUT");
+  };
+
+  useEffect(() => {
+    const getCredentials = async () => {
+      const idToken = await AsyncStorage.getItem("idToken");
+      const userId = await AsyncStorage.getItem("userId");
+      if (!idToken || !userId) {
+        logout();
+        return;
+      }
+      setAuth({ idToken, userId });
+    };
+    getCredentials();
+  }, []);
+
+  const authInterface = useMemo(() => ({
+    ...auth,
+    logout,
+    isAuthenticated,
+    storeCredentials,
+  }));
+
+  return (
+    <AuthContext.Provider value={authInterface}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  if (context === undefined) {
+    throw new Error("useAuth is not a part of the AuthProvider");
+  }
+
+  return context;
+};
+
+export { AuthProvider, useAuth };
