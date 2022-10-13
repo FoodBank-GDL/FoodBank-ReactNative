0d94fcbafb13db52ae7bc62b5c39bb6aaed44ebd
Emilio Gonzalez <emiliogonmar@Emilios-MacBook-Pro-2.local>
2022-10-12 19:41:30 -0500

    feat(map-screen): Google Maps map current location with icon

--- client/src/components/MapComponent.jsx
+++ client/src/components/MapComponent.jsx
@@ -1,22 +1,71 @@
 import { FlatList, StyleSheet, View, Dimensions } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import { useState, useEffect } from "react";
+import * as Location from 'expo-location';
+import Loading from "./Loading/Loading";
+
+import IconMI from "react-native-vector-icons/MaterialIcons"
 
 
 const MapComponent = () => {
+
+    const [position, setPosition] = useState({
+        latitude: 10,
+        longitude: 10,
+        latitudeDelta: 0.001,
+        longitudeDelta: 0.001,
+    });
+
+    const [loading, setLoading] = useState(false);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+
+            setLoading(true)
+
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+
+            setPosition({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
+            })
+
+            setLoading(false)
+        })();
+    }, []);
+
+    if (loading || errorMsg) {
+        return <Loading />
+    }
 
     return (
         <View style={Styles.container}>
             <MapView
                 provider={PROVIDER_GOOGLE}
                 style={Styles.mapStyle}
-                initialRegion={{
-                    latitude: 41.3995345,
-                    longitude: 2.1909796,
-                    latitudeDelta: 0.003,
-                    longitudeDelta: 0.003,
-                }}
+                initialRegion={position}
                 mapType="standard"
-            ></MapView>
+            >
+                <Marker
+                    key={1}
+                    coordinate={position}
+                >
+                    <IconMI
+                        name="my-location"
+                        size={30}
+                        color="#1A73E9"
+                    />
+                </Marker>
+            </MapView>
         </View>
     );
 };
@@ -25,9 +74,6 @@ const Styles = StyleSheet.create({
     container: {
         flex: 9,
         backgroundColor: "white",
-        // paddingTop: 10,
-        // paddingRight: 20,
-        // paddingLeft: 20,
     },
     mapStyle: {
         width: Dimensions.get('window').width,
