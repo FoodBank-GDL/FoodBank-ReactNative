82f855bfa23a25ca0617959ff8b1bf9bf0f4225c
CristianCazares <cm_cjavier@hotmail.com>
2022-10-13 12:13:20 -0500

    feat(client/profile): added profile placeholder

---
+++ client/src/screens/Profile/Profile.jsx
@@ -0,0 +1,19 @@
+import { StatusBar } from "../../components";
+import { View } from "react-native";
+import { Button } from "../../components";
+import { useAuth } from "../../contexts/AuthContext";
+
+const Profile = () => {
+  const { logout } = useAuth();
+  const handleLogOut = () => {
+    logout();
+  };
+  return (
+    <View>
+      <StatusBar />
+      <Button text={"Cerrar sesión"} handlePress={() => logout()} />
+    </View>
+  );
+};
+
+export default Profile;
