e9a0db16593fa74608b5939a760fe4b1fb309c53
Santiago Reyes <sannaga1995@gmail.com>
2022-10-15 21:44:49 -0500

    fix(.env): added .env file configuration to back-end

--- server/utils/firebase_config.js
+++ server/utils/firebase_config.js
@@ -2,13 +2,17 @@ const Firebase = require('firebase/app');
 const FirebaseAuth = require('firebase/auth');
 const Firestore = require('firebase/firestore');
 
+require("dotenv").config();
+
+
+console.log(process.env);
 const firebaseConfig = {
-  apiKey: "AIzaSyCsP7P7orJiW2K5-t5PwdJgi0bfNF8ufgQ",
-  authDomain: "foodbank-manager.firebaseapp.com",
-  projectId: "foodbank-manager",
-  storageBucket: "foodbank-manager.appspot.com",
-  messagingSenderId: "59102412508",
-  appId: "1:59102412508:web:0664a5f0a5ffb189387e19"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
 };
 
 const app = Firebase.initializeApp(firebaseConfig);
